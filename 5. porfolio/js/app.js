3a8cf872e31a8c4f72d1782502c19fb7539b1c27
aawww22 <57843563+oleg2201@users.noreply.github.com>
2023-11-02 14:25:53 +0500

    Update app.js

--- 5. porfolio/js/app.js
+++ 5. porfolio/js/app.js
@@ -1,30 +1,5 @@
-// const audio = document.querySelector('.audio'),
-// button = document.querySelector('.buttonisplay'),
-// nav_button = document.querySelector('nav_button')
-
-// let num = 0;
-
-// function play () {
-//     button.src = "pause.svg"
-//     audio.play()
-// }
-// function pause () {
-//     button.src = "play.svg"
-//     audio.play()
-// }
-// nav_button.addEventListener("click", () => {
-//     num++;
-//     if (num%2 !== 0 ){
-//         play()
-//     }
-//     else if (num%2 === 0) {
-//         pause()
-//     }
-
-// })
 const bt1 = document.querySelector('bt1');
-const bt2 = document.querySelector('bt2');
-const bt3 = document.querySelector('bt3');
+
 const audio = document.getElementById('audio');
 
 num = 0
@@ -39,9 +14,4 @@ const onPlay = () => {
         audio.pause()
     }
 
-}
-
-const revimg1 = () => {
-    const bt1 = document.getElementsByClassName('bt1')
-    bt1.src = './img/pause.svg'
 }
