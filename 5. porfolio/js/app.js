d809b6022a293ac5f0a0d1f84c16ff518f33f6b8
aawww22 <57843563+oleg2201@users.noreply.github.com>
2023-11-02 14:50:51 +0500

    Add files via upload

---
+++ 5. porfolio/js/app.js
@@ -0,0 +1,17 @@
+
+const bt1 = document.querySelector('bt1');
+const audio = document.getElementById('audio');
+
+num = 0
+
+const onPlay = () => {
+    num += 1
+    if (num % 2 === 0) {
+        audio.play()
+    }
+
+    else if (num % 2 > 0) {
+        audio.pause()
+    }
+
+}
